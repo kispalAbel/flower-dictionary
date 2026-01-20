4d5700a8feb29bb57dd4a22305175075cba5dfef
kispalAbel <kispal.abel2010@tanulo.boronkay.hu>
2026-01-20 12:28:50 +0100

    added some things yeaahhh

---
+++ code/navToggle.js
@@ -0,0 +1,6 @@
+const navToggle = document.getElementById("navToggle")
+    const navBar = document.getElementById("navBar")
+
+    navToggle.addEventListener("click", function() {
+        navBar.classList.toggle("open")
+    })

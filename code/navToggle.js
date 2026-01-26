822a7219020dc1ecf9f887547a3bc132f8215915
kispalAbel <kispal.abel2010@tanulo.boronkay.hu>
2026-01-26 09:02:21 +0100

    added a lota things

--- code/navToggle.js
+++ code/navToggle.js
@@ -1,6 +1,17 @@
 const navToggle = document.getElementById("navToggle")
 const navBar = document.getElementById("navBar")
 
+// Oldal betöltésekor ellenőrzés
+if (localStorage.getItem("navOpen") === "true") {
+    navBar.classList.add("open")
+} else {
+    navBar.classList.remove("open")
+}
+
 navToggle.addEventListener("click", function() {
     navBar.classList.toggle("open")
+
+    // Mentés localStorage-be
+    const isOpen = navBar.classList.contains("open")
+    localStorage.setItem("navOpen", isOpen)
 })
